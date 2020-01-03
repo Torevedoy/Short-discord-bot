bd2bafe098920bd2b244c19981db8cb0bbcb2aa2
Torevedoy <39160716+Torevedoy@users.noreply.github.com>
2020-01-03 13:34:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('message', async msg => {
-  if(!msg.author.bot) {msg.reply(('s'+('h'.repeat(Math.floor(Math.random()*100)))));});}
+    if(!msg.author.bot) {msg.reply(('s'+('h'.repeat(Math.floor(Math.random()*100)))));}});
 client.login(TOKEN);
